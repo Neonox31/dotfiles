a1aee59f825d351cb35e6fa3b49fd80af79dc978
WEBER Logan <weberlogan@gmail.com>
2019-07-25 21:06:11 +0200

    feat: add ~/.local/bin to PATH env variable

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 set fish_greeting ""
 
 # Set PATH : 
-set -gx PATH /usr/local/go/bin ./node_modules/.bin $HOME/code/go/bin $PATH
+set -gx PATH $HOME/.local/bin /usr/local/go/bin ./node_modules/.bin $HOME/code/go/bin $PATH
 
 # Set GOPATH
 set -gx GOPATH $HOME/code/go
