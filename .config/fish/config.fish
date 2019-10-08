8a682945a6b524962378ec6d144088bc117d36c8
WEBER Logan <weberlogan@gmail.com>
2019-10-08 11:04:33 +0200

    feat(fish): add configuration for autojump in config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,22 @@
 set fish_greeting ""
 
+# Set autojump
+set -gx OSTYPE (uname -a | tr '[:upper:]' '[:lower:]')
+if test -f ~/.autojump/share/autojump/autojump.fish; . ~/.autojump/share/autojump/autojump.fish; end
+
 # Set PATH : 
-set -gx PATH \
-        $HOME/.local/bin \
-        /usr/local/go/bin \
-        ./node_modules/.bin \
-        $HOME/code/go/bin \
-        /home/linuxbrew/.linuxbrew/bin \
-        $PATH
+set -gx PATH $HOME/.local/bin \
+	     ./node_modules/.bin \
+	     $HOME/.yarn/bin \
+	     $HOME/.cargo/bin \
+	     /home/linuxbrew/.linuxbrew/bin \
+	     $PATH
 
 # Set GOPATH
-set -gx GOPATH $HOME/code/go
+#set -gx GOPATH $HOME/code/go
 
 # Set JAVA_HOME
-set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
+#set -gx JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
 
 # Aliases
 ## System
